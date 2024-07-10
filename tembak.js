dfd5c8ae3b701e5863aab80fe3c33bbbd13a297b
ridhwan <jakartacodese@gmail.com>
2024-07-11 01:07:09 +0700

    coba

---
+++ tembak.js
@@ -0,0 +1,17 @@
+	var nama = "trisna";
+    var keputusan="ya";
+	
+	
+	console.log("nama kamu : ");
+	console.log(nama);
+	
+	console.log("apa kamu mau jadi pacar aku ? (ya/tidak) : ");
+	console.log(keputusan);
+		
+	if(keputusan == "ya") {
+		console.log(nama+ " ,aku mau kita meluangkan waktu berdua. ");
+	} else if (keputusan == "tidak") {
+		console.log(nama+ " ,aku minta maaf.. aku sayang sama kamu");
+		console.log("kamu tidak perlu memiliki perasaan ke aku");
+		console.log("cukup biarkan aku yang merasakannya :)");
+	}
